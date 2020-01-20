2058cf904488d3c753cba4eaba5d03b8e56f7236
Flavio Corpa <flaviocorpa@gmail.com>
2020-01-20 16:00:55 +0100

    Read primitive Schema types 🦖

--- src/Language/Avro/Types.hs
+++ src/Language/Avro/Types.hs
@@ -1,5 +1,13 @@
 module Language.Avro.Types where
 
+import qualified Data.Text as T
+
+data TypeName
+  = TN
+      { baseName :: T.Text,
+        namespace :: [T.Text]
+      }
+
 data ImportType
   = IdlImport
   | ProtocolImport
