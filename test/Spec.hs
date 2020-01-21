3a8eb7badeedf2750c142c16522477d272c90288
Flavio Corpa <flavio.corpa@47deg.com>
2020-01-21 16:37:38 +0100

    Simplify tests 👨🏼‍🔬

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,15 +5,11 @@ module Main
   )
 where
 
-import Control.Applicative hiding (some)
-import Data.Text (Text)
-import Data.Void
-import qualified Language.Avro.Parser as P
+import Language.Avro.Parser
 import Language.Avro.Types
 import Test.Hspec
 import Test.Hspec.Megaparsec
 import Text.Megaparsec
-import Text.Megaparsec.Char
 
 main :: IO ()
 main =
@@ -21,16 +17,16 @@ main =
     $ describe "Parse annotations"
     $ do
       it "should parse namespaces" $
-        parse P.parseAnnotation "" "@namespace(\"mynamespace\")"
+        parse parseAnnotation "" "@namespace(\"mynamespace\")"
           `shouldBe` (Right $ Namespace "mynamespace")
       it "should parse ordering" $
-        parse P.parseAnnotation "" "@order(\"ascending\")"
+        parse parseAnnotation "" "@order(\"ascending\")"
           `shouldBe` (Right $ Order "ascending")
       it "should parse aliases" $
-        parse P.parseAnnotation "" "@aliases([\"org.old.OldRecord\", \"org.ancient.AncientRecord\"])"
+        parse parseAnnotation "" "@aliases([\"org.old.OldRecord\", \"org.ancient.AncientRecord\"])"
           `shouldBe` (Right $ Aliases ["org.old.OldRecord", "org.ancient.AncientRecord"])
       it "should parse other annotations" $ do
-        parse P.parseAnnotation "" "@java-class(\"java.util.ArrayList\")"
+        parse parseAnnotation "" "@java-class(\"java.util.ArrayList\")"
           `shouldBe` (Right $ OtherAnnotation "java-class" "java.util.ArrayList")
-        parse P.parseAnnotation "" "@java-key-class(\"java.io.File\")"
+        parse parseAnnotation "" "@java-key-class(\"java.io.File\")"
           `shouldBe` (Right $ OtherAnnotation "java-key-class" "java.io.File")
