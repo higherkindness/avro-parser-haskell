c568e8720540037854cf5ae15c9be44c056c7176
Flavio Corpa <flaviocorpa@gmail.com>
2020-01-22 11:23:51 +0100

    Test imports ⚗️

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # avro-parser-haskell
 
 [![Actions Status](https://github.com/kutyel/avro-parser-haskell/workflows/Haskell%20CI/badge.svg)](https://github.com/kutyel/avro-parser-haskell/actions)
+[![ormolu](https://img.shields.io/badge/styled%20with-ormolu-blueviolet)](https://github.com/tweag/ormolu)
 
 Language definition and parser for AVRO (`.avdl`) files.
--- test/Spec.hs
+++ test/Spec.hs
@@ -12,21 +12,29 @@ import Test.Hspec.Megaparsec
 import Text.Megaparsec
 
 main :: IO ()
-main =
-  hspec
-    $ describe "Parse annotations"
-    $ do
-      it "should parse namespaces" $
-        parse parseAnnotation "" "@namespace(\"mynamespace\")"
-          `shouldBe` (Right $ Namespace "mynamespace")
-      it "should parse ordering" $
-        parse parseAnnotation "" "@order(\"ascending\")"
-          `shouldBe` (Right $ Order "ascending")
-      it "should parse aliases" $
-        parse parseAnnotation "" "@aliases([\"org.old.OldRecord\", \"org.ancient.AncientRecord\"])"
-          `shouldBe` (Right $ Aliases ["org.old.OldRecord", "org.ancient.AncientRecord"])
-      it "should parse other annotations" $ do
-        parse parseAnnotation "" "@java-class(\"java.util.ArrayList\")"
-          `shouldBe` (Right $ OtherAnnotation "java-class" "java.util.ArrayList")
-        parse parseAnnotation "" "@java-key-class(\"java.io.File\")"
-          `shouldBe` (Right $ OtherAnnotation "java-key-class" "java.io.File")
+main = hspec $ do
+  describe "Parse annotations" $ do
+    it "should parse namespaces" $
+      parse parseAnnotation "" "@namespace(\"mynamespace\")"
+        `shouldBe` (Right $ Namespace "mynamespace")
+    it "should parse ordering" $
+      parse parseAnnotation "" "@order(\"ascending\")"
+        `shouldBe` (Right $ Order "ascending")
+    it "should parse aliases" $
+      parse parseAnnotation "" "@aliases([\"org.old.OldRecord\", \"org.ancient.AncientRecord\"])"
+        `shouldBe` (Right $ Aliases ["org.old.OldRecord", "org.ancient.AncientRecord"])
+    it "should parse other annotations" $ do
+      parse parseAnnotation "" "@java-class(\"java.util.ArrayList\")"
+        `shouldBe` (Right $ OtherAnnotation "java-class" "java.util.ArrayList")
+      parse parseAnnotation "" "@java-key-class(\"java.io.File\")"
+        `shouldBe` (Right $ OtherAnnotation "java-key-class" "java.io.File")
+  describe "Parse imports" $ do
+    it "should parse idl" $
+      parse parseImport "" "import idl \"foo.avdl\";"
+        `shouldBe` (Right $ IdlImport "foo.avdl")
+    it "should parse protocol" $
+      parse parseImport "" "import protocol \"foo.avpr\";"
+        `shouldBe` (Right $ ProtocolImport "foo.avpr")
+    it "should parse schema" $
+      parse parseImport "" "import schema \"foo.avsc\";"
+        `shouldBe` (Right $ SchemaImport "foo.avsc")
