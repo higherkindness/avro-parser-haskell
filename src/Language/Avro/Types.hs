012f91f910c617eeafce2f65b7b2d08204f4ecb9
Flavio Corpa <flaviocorpa@gmail.com>
2020-02-20 08:29:26 +0000

    Use Set as the underlying data structure ⛩ (#3)

--- src/Language/Avro/Types.hs
+++ src/Language/Avro/Types.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE StandaloneDeriving #-}
+
 -- | Language definition for AVRO (@.avdl@) files,
 --   as defined in <http://avro.apache.org/docs/1.8.2/spec.html>.
 module Language.Avro.Types
@@ -7,7 +9,8 @@ module Language.Avro.Types
 where
 
 import Data.Avro.Schema
-import Data.List (nub)
+import Data.Avro.Types.Value
+import Data.Set
 import qualified Data.Text as T
 
 -- | Whole definition of protocol.
@@ -15,25 +18,43 @@ data Protocol
   = Protocol
       { ns :: Maybe Namespace,
         pname :: T.Text,
-        imports :: [ImportType],
-        types :: [Schema],
-        messages :: [Method]
+        imports :: Set ImportType,
+        types :: Set Schema,
+        messages :: Set Method
       }
-  deriving (Eq, Show)
+  deriving (Eq, Show, Ord)
 
 instance Semigroup Protocol where
   p1 <> p2 =
     Protocol
       (ns p1)
       (pname p1)
-      (nub (imports p1 <> imports p2))
-      (nub (types p1 <> types p2))
-      (nub (messages p1 <> messages p2))
+      (imports p1 <> imports p2)
+      (types p1 <> types p2)
+      (messages p1 <> messages p2)
+
+deriving instance Ord LogicalTypeInt
+
+deriving instance Ord LogicalTypeLong
+
+deriving instance Ord LogicalTypeBytes
+
+deriving instance Ord Decimal
+
+deriving instance Ord LogicalTypeString
+
+deriving instance Ord LogicalTypeFixed
+
+deriving instance (Ord a) => Ord (Value a)
+
+deriving instance Ord Field
+
+deriving instance Ord Schema
 
 -- | Newtype for the namespace of methods and protocols.
 newtype Namespace
   = Namespace [T.Text]
-  deriving (Eq, Show)
+  deriving (Eq, Show, Ord)
 
 type Aliases = [TypeName]
 
@@ -50,7 +71,7 @@ data ImportType
   = IdlImport T.Text
   | ProtocolImport T.Text
   | SchemaImport T.Text
-  deriving (Eq, Show)
+  deriving (Eq, Show, Ord)
 
 -- | Helper type for the arguments of 'Method'.
 data Argument
@@ -58,7 +79,7 @@ data Argument
       { atype :: Schema,
         aname :: T.Text
       }
-  deriving (Eq, Show)
+  deriving (Eq, Show, Ord)
 
 -- | Type for methods/messages that belong to a protocol.
 data Method
@@ -69,4 +90,4 @@ data Method
         throws :: Schema,
         oneway :: Bool
       }
-  deriving (Eq, Show)
+  deriving (Eq, Show, Ord)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main
